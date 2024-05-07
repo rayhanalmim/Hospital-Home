d7cf6063cd62ebead1c34da4af7dc7ad1e6c53e5
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-07 13:24:36 +0600

    creating project using react and setup tailwind css and react router dom

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3>hello this is home</h3>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Route/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Root></Root>
+    }
+])
+
+export default Router;
