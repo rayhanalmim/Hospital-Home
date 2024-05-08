8a6a6f5dfc54f2c92d9556a1168a6774e37ee9d7
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-08 23:09:52 +0600

    create a custom animation for faq sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spin: {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
+        },
+        flooding3: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-15px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        flooding4: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(15px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        spin: 'spin 14s linear infinite',
+        flooding3: 'flooding3 2s ease-in-out infinite',
+        flooding4: 'flooding4 2s ease-in-out infinite',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
   ],
-}
+};
+
 
